152cd0c1e1cebcc36ee1343a4a63ddec53959892
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2024-12-09 21:25:33 +0000

    WIP

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GalleryItem, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,175 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  images: GalleryItem[] = [
+    new ImageItem({
+      src: 'assets/images/xmas.jpg',
+      thumb: 'assets/images/xmas.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    new ImageItem({
+      src: 'assets/images/xmax2.jpg',
+      thumb: 'assets/images/xmax2.jpg'
+    }),
+    
+  ];
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { GalleryModule } from 'ng-gallery';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    GalleryModule
   ],
-  providers: [],
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(GalleryModule)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
