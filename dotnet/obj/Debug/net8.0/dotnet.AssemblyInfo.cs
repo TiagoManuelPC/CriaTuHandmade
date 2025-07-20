f76633845b50b7c767ef4430f4e0f20d283c2b27
Tiago Manuel <tiagomanuelpereiracampos@hotmail.com>
2025-07-20 23:46:37 +0100

    add git ignore file, added connection string

--- dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
+++ dotnet/obj/Debug/net8.0/dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584c47bd0dc892753e59cbd10d90d3929077c1f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2889af4dde48ee27c1f26f6aa5c84885fae4eaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
